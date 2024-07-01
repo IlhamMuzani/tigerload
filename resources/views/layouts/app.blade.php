eb13ac3d0a99a58b3548060eae1f15eb75ec0263
IlhamMuzani <ilhammuzani21@gmail.com>
2024-07-01 16:37:10 +0700

    hide pusher:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
-    <script src="{{ asset('js/pusher.js') }}"></script>
+    {{-- <script src="{{ asset('js/pusher.js') }}"></script> --}}
 </head>
 
 <body class="hold-transition sidebar-mini @if (request()->is('admin/perhitungan_gaji*') ||
@@ -180,7 +180,7 @@
     <!-- AdminLTE App -->
     <script src="{{ asset('adminlte/dist/js/adminlte.js') }}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('adminlte/dist/js/pages/dashboard.js') }}"></script>
+    {{-- <script src="{{ asset('adminlte/dist/js/pages/dashboard.js') }}"></script> --}}
 
     <!-- DataTables  & Plugins -->
     <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
