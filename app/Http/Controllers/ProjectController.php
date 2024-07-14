9f6fc242caaa14e464e807a2eb43512be7a706d2
IlhamMuzani <ilhammuzani21@gmail.com>
2024-07-14 08:38:39 +0700

    perbaikan cetak

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,7 +8,28 @@ use Illuminate\Contracts\Encryption\DecryptException;
 
 class ProjectController extends Controller
 {
-    public function detail($encryptedId)
+    // public function detail($encryptedId)
+    // {
+    //     try {
+    //         // Dekripsi ID
+    //         $id = Crypt::decryptString($encryptedId);
+    //     } catch (DecryptException $e) {
+    //         // Tangani kesalahan jika ID tidak dapat didekripsi
+    //         return abort(404, 'Invalid encrypted ID');
+    //     }
+
+    //     // Retrieve the main record
+    //     $cetakpdf = Project::where('id', $id)->first();
+
+    //     // Check if the main record exists
+    //     if (!$cetakpdf) {
+    //         return abort(404, 'Project not found');
+    //     }
+
+    //     return view('admin.project.detail', compact('cetakpdf'));
+    // }
+
+    public function qrcode_detail($encryptedId)
     {
         try {
             // Dekripsi ID
