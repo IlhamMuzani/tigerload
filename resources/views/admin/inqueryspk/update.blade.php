570048bfa1cd7f61fbc059238f66f8e0c50bdec8
IlhamMuzani <ilhammuzani21@gmail.com>
2024-09-21 14:53:52 +0700

    perbaikan inquery surat pesanan

--- resources/views/admin/inqueryspk/update.blade.php
+++ resources/views/admin/inqueryspk/update.blade.php
@@ -8,11 +8,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Inquery SPK</h1>
+                    <h1 class="m-0">Inquery Surat Pemesanan Kendaraan</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('admin/spk') }}">SPK</a></li>
+                        <li class="breadcrumb-item"><a href="{{ url('admin/spk') }}">Surat Pemesanan Kendaraan</a></li>
                         <li class="breadcrumb-item active">Inquery</li>
                     </ol>
                 </div><!-- /.col -->
--- resources/views/admin/spk/create.blade.php
+++ resources/views/admin/spk/create.blade.php
@@ -27,7 +27,7 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('admin/spk') }}">SPK</a></li>
+                        <li class="breadcrumb-item"><a href="{{ url('admin/spk') }}">Surat Pemesanan Kendaraan</a></li>
                         <li class="breadcrumb-item active">Tambah</li>
                     </ol>
                 </div><!-- /.col -->
